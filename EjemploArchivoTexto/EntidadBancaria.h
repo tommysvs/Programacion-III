b6571a59ff6a857ee58b1bc7cb5015372716f252
TOMMY STANLEY VEGA SERVELLON <tommysvs@unitec.edu>
2020-09-07 09:27:17 -0600

    Semana 8 - Día 1

---
+++ EjemploArchivoTexto/EntidadBancaria.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#ifndef ENTIDAD_BANCARIA_H
+#define ENTIDAD_BANCARIA_H
+
+class EntidadBancaria {
+private:
+
+public:
+	static void ingresarCliente();
+	static void consultarClientes();
+};
+
+#endif // !ENTIDAD_BANCARIA_H
---
+++ EjemploArchivoTexto/main.cpp
@@ -0,0 +1,9 @@
+#include <conio.h>
+#include "EntidadBancaria.h"
+
+int main() {
+	EntidadBancaria::ingresarCliente();
+	EntidadBancaria::consultarClientes();
+
+	_getch();
+}
